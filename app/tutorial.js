646222ca7981f98cc1b327b2d28ec7d4e6914f07
Mark Thompson <mark.thompson@smpl.company>
2020-11-09 13:55:34 -0800

    Object to primitive conversion

--- app/tutorial.js
+++ app/tutorial.js
@@ -1,21 +1,39 @@
 'use strict';
-const cowSymbol = Symbol('cow');
 
+// const test = new function MooCow() {
 const test = {
-    moo: 'moo moo moo',
-    [cowSymbol]: 'cow cow cow',
+    name: 'moo cow',
+    // this.name = 'moo cow';
+
+    [Symbol.toPrimitive](hint) {
+        console.log(hint);
+        return hint === 'string' ? this._toString() : this._valueOf();
+    },
+
+    _toString() {
+        return this.name;
+    },
+
+    toString() {
+        return this[Symbol.toPrimitive]('string');
+    },
+
+    _valueOf() {
+        return 9;
+    },
+
+    valueOf() {
+        return this[Symbol.toPrimitive]('number');
+    },
 };
 
 console.log(test);
-console.log(test.moo);
-console.log(test.cow);
-console.log(test[cowSymbol]);
-
-for (const key in test) {
-    console.log(key, ',', test[key]);
-}
-
-const symbols = Object.getOwnPropertySymbols(test);
-
-console.log(symbols);
-console.log(test[symbols[0]]);
+console.log(test + " str test");
+console.log(test + 4);
+console.log("str test " + test);
+console.log(4 + test);
+console.log(test * 2);
+console.log(2 * test);
+console.log(String(test));
+console.log(Number(test));
+console.log(Boolean(test));
