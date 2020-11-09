fdcd78063940bd24d0ed25086b69ed47911238e5
Mark Thompson <mark.thompson@smpl.company>
2020-11-08 18:47:14 -0800

    Objects

--- app/test.js
+++ app/test.js
@@ -1,50 +1,80 @@
-let assert = chai.assert;
 
-describe("pow", function() {
+// <!-- add mocha css, to show results -->
+// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mocha/3.2.0/mocha.css">
 
-    it("2 raised to power 3 is 8", function() {
-        assert.equal(pow(2, 3), 8);
-    });
+// <!-- add mocha framework code -->
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/mocha/3.2.0/mocha.js"></script>
 
-    it("3 raised to power 4 is 81", function() {
-        assert.equal(pow(3, 4), 81);
-    });
+// <script>
+//     mocha.setup('bdd'); // minimal setup
+// </script>
 
-    describe("raises x to power 3", function() {
+// <!-- add chai -->
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/chai/3.5.0/chai.js"></script>
 
-        function makeTest(x) {
-            let expected = x * x * x;
+// ...
+
+// <!-- the element with id="mocha" will contain test results -->
+//     <div id="mocha"></div>
+
+//     <script src="/tutorial.js"></script>
+
+//     <!-- <script src="/test.js"></script> -->
+
+//     <!-- run tests! -->
+//     <script>
+//         mocha.run();
+//     </script>
+
+
+
+// let assert = chai.assert;
+
+// describe("pow", function() {
+
+//     it("2 raised to power 3 is 8", function() {
+//         assert.equal(pow(2, 3), 8);
+//     });
+
+//     it("3 raised to power 4 is 81", function() {
+//         assert.equal(pow(3, 4), 81);
+//     });
+
+//     describe("raises x to power 3", function() {
+
+//         function makeTest(x) {
+//             let expected = x * x * x;
     
-            it(`${x} in the power 3 is ${expected}`, function() {
-                assert.equal(pow(x, 3), expected);
-            });
-        }
+//             it(`${x} in the power 3 is ${expected}`, function() {
+//                 assert.equal(pow(x, 3), expected);
+//             });
+//         }
     
-        for (let x = 1; x <= 5; x++) {
-            makeTest(x);
-        }
+//         for (let x = 1; x <= 5; x++) {
+//             makeTest(x);
+//         }
     
-    });
+//     });
 
-    it("for negative n the result is NaN", function() {
-        assert.isNaN(pow(2, -1));
-    });
+//     it("for negative n the result is NaN", function() {
+//         assert.isNaN(pow(2, -1));
+//     });
 
-    it("for non-integer n the result is NaN", function() {
-        assert.isNaN(pow(2, 1.5));
-    });
-  
-});
-
-// describe("test", function() {
-
-//     before(() => alert("Testing started – before all tests"));
-//     after(() => alert("Testing finished – after all tests"));
-  
-//     beforeEach(() => alert("Before a test – enter a test"));
-//     afterEach(() => alert("After a test – exit a test"));
-  
-//     it('test 1', () => alert(1));
-//     it('test 2', () => alert(2));
+//     it("for non-integer n the result is NaN", function() {
+//         assert.isNaN(pow(2, 1.5));
+//     });
   
 // });
+
+// // describe("test", function() {
+
+// //     before(() => alert("Testing started – before all tests"));
+// //     after(() => alert("Testing finished – after all tests"));
+  
+// //     beforeEach(() => alert("Before a test – enter a test"));
+// //     afterEach(() => alert("After a test – exit a test"));
+  
+// //     it('test 1', () => alert(1));
+// //     it('test 2', () => alert(2));
+  
+// // });
--- app/tutorial.js
+++ app/tutorial.js
@@ -1,16 +1,71 @@
 'use strict';
 
-function pow(x, n) {
-    // return x**n;
+// const user = {};
 
-    if (n < 0) return NaN;
-    if (Math.round(n) != n) return NaN;
+// user.name = "John";
+// user.surname = "Smith";
 
-    let result = 1;
+// debugger;
 
-    for (let i = 0; i < n; i++) {
-        result *= x;
+// user.name = "Pete";
+
+// debugger;
+
+
+// delete user["name"];
+
+// debugger;
+
+// console.log(user);
+
+
+
+// function isEmpty(obj) {
+//     for (let key in obj) {
+//         return false;
+//     }
+
+//     return true;
+// }
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130,
+// };
+
+// let sum = 0;
+
+// for (let key in salaries) {
+//     sum += salaries[key] || 0;
+// }
+
+// console.log(sum);
+
+// before the call
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+  
+multiplyNumeric(menu);
+  
+// // after the call
+// menu = {
+//     width: 400,
+//     height: 600,
+//     title:
+ "My menu"
+// };
+console.log(menu);
+
+function multiplyNumeric(obj) {
+    for (const key in obj) {
+        if (typeof obj[key] === 'number') {
+            obj[key] = obj[key] * 2;
+        }
     }
 
-    return result;
+    return obj;
 }
