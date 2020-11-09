d0a22c7c62e7ccd3d58ac0edf6ba0ec4bbe9fcba
Mark Thompson <mark.thompson@smpl.company>
2020-11-09 13:36:43 -0800

    Symbol type

--- app/tutorial.js
+++ app/tutorial.js
@@ -1,93 +1,21 @@
 'use strict';
+const cowSymbol = Symbol('cow');
 
-// function User(name) {
-//     // this = {};  (implicitly)
-  
-//     // add properties to this
-//     this.name = name;
-//     this.isAdmin = false;
-  
-//     // return this;  (implicitly)
-// }
+const test = {
+    moo: 'moo moo moo',
+    [cowSymbol]: 'cow cow cow',
+};
 
-// const test = new User('Mark');
-// console.log(test);
+console.log(test);
+console.log(test.moo);
+console.log(test.cow);
+console.log(test[cowSymbol]);
 
-// let test2;
-
-// {
-//     function User(name) {
-//         // this = {};  (implicitly)
-      
-//         // add properties to this
-//         this.name = name;
-//         this.isAdmin = false;
-      
-//         // return this;  (implicitly)
-
-//         this.other = "Some other User constructor";
-//     }
-
-//     test2 = new User('Mark 2');
-// }
-
-// console.log(test2);
-
-// const moo = {
-//     cow() {
-//         return this.text;
-//     },
-//     text: 'I am cow',
-// };
-
-// console.log(moo.cow());
-
-
-
-
-// const someObject = {
-//     name: "some object",
-// };
-
-// function A() { return someObject }
-// function B() { return someObject }
-
-// let a = new A;
-// let b = new B;
-
-// alert( a == b ); // true
-
-// function Calculator() {
-//     this.read = function() {
-//         this.a = +prompt('a?', '');
-//         this.b = +prompt('b?', '');
-//     };
-
-//     this.sum = function() {
-//         return this.a + this.b;
-//     };
-
-//     this.mul = function() {
-//         return this.a * this.b;
-//     };
-// }
-
-// let calculator = new Calculator();
-// calculator.read();
-
-// alert( "Sum=" + calculator.sum() );
-// alert( "Mul=" + calculator.mul() );
-
-function Accumulator(startingValue) {
-    this.value = startingValue;
-    this.read = function() {
-        this.value += +prompt("new value?", "");
-    };
+for (const key in test) {
+    console.log(key, ',', test[key]);
 }
 
-let accumulator = new Accumulator(1); // initial value 1
+const symbols = Object.getOwnPropertySymbols(test);
 
-accumulator.read(); // adds the user-entered value
-accumulator.read(); // adds the user-entered value
-
-alert(accumulator.value); // shows the sum of these values
+console.log(symbols);
+console.log(test[symbols[0]]);
