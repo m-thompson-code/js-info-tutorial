3d768cbc03966c87fa51321f9b329afa86ac16de
Mark Thompson <mark.thompson@smpl.company>
2020-11-09 14:44:40 -0800

    String

--- app/tutorial.js
+++ app/tutorial.js
@@ -1,33 +1,15 @@
-// 'use strict';
+'use strict';
 
-// // Create a script that prompts the visitor to enter two numbers
-// // and then shows their sum.
-// function sum(a, b) {
-//     return a + b;
-// }
+// console.log('a'.localeCompare('b'));
+// console.log('b'.localeCompare('a'));
+// console.log('a'.localeCompare('a'));
 
-// console.log(sum(
-//     +prompt('a?', ''),
-//     +prompt('b?', ''),
-// ));
+// console.log('a'.localeCompare('B'));
+// console.log('b'.localeCompare('A'));
+// console.log('a'.localeCompare('A'));
 
-// console.log(6.35.toFixed(1));
-// console.log(Math.round(6.35 * 10) / 10);
-// const n = 6.35;
-// console.log(n.toFixed(20));
-// console.log((n * 10).toFixed(20));
-// console.log(Math.round(n * 10) / 10);
-
-console.log(typeof Infinity);
-console.log(typeof NaN);
-
-let i = 0;
-let j = 0;
-while (i != 10) {
-  i += 0.2;
-  console.log(i);
-  j++;
-  if (j > 50) {
-      break;
-  }
+function ucFirst(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1, str.length);
 }
+
+console.log(ucFirst("john") == "John");
