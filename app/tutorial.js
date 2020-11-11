822e25fd30fe3e1915a3503ac1d2afa232cff27b
Mark Thompson <mark.thompson@smpl.company>
2020-11-10 17:14:20 -0800

    Prototype methods, objects without __proto__

--- app/tutorial.js
+++ app/tutorial.js
@@ -1,36 +1,54 @@
 'use strict';
 
-// let obj = {};
+// let dictionary = Object.create(null, {
+//     toString: {
+//         // value: function() {
+//         //     let str = '';
 
-// console.log(obj);
-// window.test_obj = obj;
+//         //     for (let key in dictionary) {
+//         //         if (str) {
+//         //             str += ',';
+//         //         }
+//         //         str += key;
+//         //     }
 
-// console.log(obj.__proto__ === Object.prototype); // true
+//         //     return str;
+//         // },
+//         value: function() {
+//             return Object.keys(this).join(',');
+//         },
+//     }
+// });
 
-// console.log(obj.toString === obj.__proto__.toString); //true
-// console.log(obj.toString === Object.prototype.toString); //true
+// // add some data
+// dictionary.apple = "Apple";
+// dictionary.__proto__ = "test"; // __proto__ is a regular property key here
 
-// function f() {
-//     alert("Hello!");
+// // only apple and __proto__ are in the loop
+// for(let key in dictionary) {
+//   alert(key); // "apple", then "__proto__"
 // }
 
-function f(a, b) {
-    alert( a + b );
+// // your toString in action
+// alert(dictionary); // "apple,__proto__"
+
+// console.log(dictionary);
+
+function Rabbit(name) {
+    this.name = name;
 }
+Rabbit.prototype.sayHi = function() {
+    alert(this.name);
+};
+  
+let rabbit = new Rabbit("Rabbit");
 
-Function.prototype.defer = function(ms) {
-    // const that = this;
-    // return function(...args) {
-    //     console.log(that);
-    //     setTimeout(that.apply(that, args), ms);
-    // };
 
-    // const that = this;
-    return (...args) => {
-        console.log(this);
-        setTimeout(this.apply(this, args), ms);
-    };
-}
+// rabbit.sayHi();// "Rabbit" // this is rabbit
+// Rabbit.prototype.sayHi(); // undefined (this refers to no one in the Rabbit.prototype.sayHi)
+// Object.getPrototypeOf(rabbit).sayHi(); // Same as above
+// rabbit.__proto__.sayHi(); // rabbit.__proto__ is Rabbit,
 
-// f.defer(1000); // shows "Hello!" after 1 second
-f.defer(1000)(1, 2); // shows 3 after 1 second
+console.log(Rabbit.prototype.constructor == Rabbit);
+console.log(Rabbit.prototype == Object.getPrototypeOf(rabbit));
+console.log(rabbit.__proto__ == Rabbit.prototype);
