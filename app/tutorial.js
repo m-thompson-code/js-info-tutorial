858818117d16976d8ca6d04b4de86cde7ec43c19
Mark Thompson <mark.thompson@smpl.company>
2020-11-10 20:02:03 -0800

    Attributes and properties

--- app/tutorial.js
+++ app/tutorial.js
@@ -1,45 +1,31 @@
 'use strict';
 
-// Notice how when setting ids for a div, global (window) stores a referenec to this div by its id
-// console.log(moo);
+// const element = document.querySelector('[data-widget-name]');
+const element = document.querySelector('[data-widget-name="menu"]');
 
-// // document.getElementsByTagName is a Live Collection
-// const divs = document.getElementsByTagName('div');
+console.log(element);
 
-// // The contents of divs will always be updated when the query changes
+const widgetName = element.dataset.widgetName;
 
-// // const interval = new function() {
-// //     this.act = function() {
-// //         setTimeout(() => {
-// //             const newDiv = document.createElement('div');
-// //             newDiv.innerText = `Another one (${})`;
-// //             document.body.appendChild(newDiv);
-// //             console.log(divs);
-// //             this.act();
-// //         }, 1000);
-// //     };
-// // };
+console.log(widgetName);
 
-// // interval.act();
+// setting style for a single link
+const links = Array.from(document.querySelectorAll('a'));
 
-// setInterval(() => {
-//     const newDiv = document.createElement('div');
-//     newDiv.id = `another-one-${divs.length}`;
-//     newDiv.innerText = `Another one (${divs.length})`;
-//     document.body.appendChild(newDiv);
-//     // Notice here, we aren't performing document.getELementsByTagName again, 
-//     // just using the reference to divs. However the Array-like is keeping up to date with the new divs
-//     console.log(divs);
-// }, 1000);
+links.forEach(link => {
+    console.log(link.href);
+    console.log(link.getAttribute('href'));
+    const href = link.getAttribute('href');
 
-const table = document.getElementById('age-table');
-console.log(table);
-console.log(table.getElementsByTagName('label'));// document.querySelectorAll('#age-table label')
-console.log(table.getElementsByTagName('td')[0], table.querySelector('td'), table.rows[0].cells[0]);
-console.log(document.querySelector('form[name="search"]'), Array.from(document.getElementsByTagName('form')).filter(form => form.getAttribute('name')==='search')[0]);
+    if (!href) {
+        return;
+    }
 
-const form = document.querySelector('form[name="search"]');
-console.log(form.querySelector('input'));
+    if (href.startsWith('http://internal.com')) {
+        return;
+    }
 
-const inputs = form.querySelectorAll('input');
-console.log(inputs[inputs.length - 1]);
+    if (href.includes('://')) {
+        link.style.color = 'orange'
+    }
+});
