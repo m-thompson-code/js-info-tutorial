024437a1d1a6bfbf03e8e06e191f788faf3a4a6a
Mark Thompson <mark.thompson@smpl.company>
2020-11-11 00:40:23 -0800

    some notes after skimming events

--- app/tutorial.js
+++ app/tutorial.js
@@ -1,44 +1,29 @@
 'use strict';
 
-function removeNotification(element) {
-    element.remove();
+function hideText() {
+    document.getElementById('text')?.remove();
 }
 
-function showNotification(options) {
-    const top = options?.top || 0;
-    const right = options?.right || 0;
-    const html = options?.html || '';
-    const className = options?.className || '';
+const div = document.querySelector('div');
+const button = document.querySelector('button');
 
-    const div = document.createElement('div');
+div.addEventListener("click", (event) => {
+    console.log(event.target, event.currentTarget);
+    console.log("1");
+});
+button.addEventListener("click", (event) => {
+    console.log(event.target, event.currentTarget);
+    console.log("1");
+});
 
-    div.style.marginTop = `${top || 0}px`;
-    div.style.marginRight = `${right || 0}px`;
-
-    if (html) {
-        div.innerHTML = html;
-    }
-
-    div.className = `notification`;
-    if (className) {
-        div.classList.add(className);
-    }
-
-    document.body.append(div);
-
-    setTimeout(() => {
-        removeNotification(div);
-    }, 1500);
-}
-
-function test() {
-    // shows an element with the text "Hello" near the right-top of the window
-    showNotification({
-        top: 10, // 10px from the top of the window (by default 0px)
-        right: 10, // 10px from the right edge of the window (by default 0px)
-        html: "Hello!", // the HTML of notification
-        className: "welcome" // an additional class for the div (optional)
-    });
-}
-
-test();
+div.addEventListener("click", (event) => {
+    console.log(event.target, event.currentTarget);
+    console.log("2");
+});
+button.addEventListener("click", (event) => {
+    console.log(event.target, event.currentTarget);
+    console.log("2");
+    
+    // event.stopImmediatePropagation();
+    event.stopPropagation();
+});
